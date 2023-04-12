f171ae0aab323b0055cf36b980dbcab1db8af3e4
berat.zorlu <berat.zorlu@hotmail.com>
2023-04-12 19:41:27 +0000

    Add loop to display item name and done status.

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Item
 
 
 # Create your views here.
 def get_todo_list(request):
-    return render(request, "todo/todo_list.html")
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    # This will ensure complete communication between frontend (users) and
+    # backend (db).
+    return render(request, "todo/todo_list.html", context)
