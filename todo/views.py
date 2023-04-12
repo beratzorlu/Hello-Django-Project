78cd8e2e90a290bcebdfa193a41330716a03bf24
berat.zorlu <berat.zorlu@hotmail.com>
2023-04-12 20:38:09 +0000

    Add item add and display functionality.

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 
@@ -6,7 +6,7 @@ from .models import Item
 def get_todo_list(request):
     items = Item.objects.all()
     context = {
-        'items': items
+        "items": items
     }
     # This will ensure complete communication between frontend (users) and
     # backend (db).
@@ -14,4 +14,9 @@ def get_todo_list(request):
 
 
 def add_item(request):
+    if request.method == "POST":
+        name = request.POST.get("item_name")
+        done = "done" in request.POST
+        Item.objects.create(name=name, done=done)
+        return redirect("get_todo_list")
     return render(request, "todo/add_item.html")
